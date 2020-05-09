1a45ae216609d02f63b2555d78b27f5edefef791
Marcelo Garnier <mgarnier@gmail.com>
2020-05-09 13:14:26 -0300

    Alterações nas classes de Planejamento para utilização da classe Periodicidade.

--- 7182-master/Novo/ISUB.Test/Repositories/FakePlanejamentoRepository.cs
+++ 7182-master/Novo/ISUB.Test/Repositories/FakePlanejamentoRepository.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ISUB.Domain;
 using ISUB.Domain.Entities;
 using ISUB.Domain.Enum;
 using ISUB.Domain.Repositories.Interface;
+using System;
+using System.Collections.Generic;
 
 namespace ISUB.Test.Repositories
 {
@@ -11,12 +11,12 @@ namespace ISUB.Test.Repositories
     {
         private Procedimento _procedimento = new Procedimento("TEADF", 12, AreaNegocio.Flexivel, true);
         //private Objeto _objeto = new Objeto(AreaNegocio.Flexivel, "TR500101");
-        private List<Objeto> _objetos = new List<Objeto>() { new Objeto(AreaNegocio.Flexivel, "TR500101")};
+        private List<Objeto> _objetos = new List<Objeto>() { new Objeto(AreaNegocio.Flexivel, "TR500101") };
         //_objetos.Add(_objeto);
 
         public Planejamento Get(Guid Id)
         {
-            var planejamento = new Planejamento(_objetos, _procedimento, new DateTime(2018, 01, 15), 12, TipoPeriodicidade.Mes);
+            var planejamento = new Planejamento(_objetos, _procedimento, new DateTime(2018, 01, 15), new Periodicidade(12));
             return planejamento;
         }
 
