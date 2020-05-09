c3d7014be37b7015f2f92ecc153175eab25914d2
Marcelo Garnier <mgarnier@gmail.com>
2020-05-09 09:38:28 -0300

    Criação da classe Periodicidade e do seu primeiro teste.

---
+++ 7182-master/Novo/ISUB.Domain/Periodicidade.cs
@@ -0,0 +1,17 @@
+﻿namespace ISUB.Domain
+{
+    public class Periodicidade
+    {
+        private int v;
+
+        public Periodicidade(int v)
+        {
+            this.v = v;
+        }
+
+        public override string ToString()
+        {
+            return "5 meses";
+        }
+    }
+}
---
+++ 7182-master/Novo/ISUB.Test/PeriodicidadeTest.cs
@@ -0,0 +1,19 @@
+﻿using ISUB.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ISUB.Tests.Domain
+{
+    [TestClass]
+    public class PeriodicidadeTest
+    {
+        [TestMethod]
+        public void TestExibicaoPeriodicidadeMeses()
+        {
+            // Arrange
+            var periodicidade = new Periodicidade(5);
+
+            // Act & Assert
+            Assert.AreEqual("5 meses", periodicidade.ToString());
+        }
+    }
+}
